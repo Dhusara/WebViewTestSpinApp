f1a6b8650629e9527d8eae148e01d25a687422e8
Sergei Demianenko <siriysgrey@gmail.com>
2020-09-12 22:42:18 +0300

    first commit

---
+++ WebView/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  WebView
+//
+//  Created by Serhii Demianenko on 10.09.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import Foundation
